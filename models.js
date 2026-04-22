ad5c4ba8fcd6151b7105039ad651267427a25e18
Joca <joca@planaltonet.net.br>
2026-04-22 11:41:58 -0300

    Ajuste menu compacto e organizacao do models.js

--- models.js
+++ models.js
@@ -1,14 +1,54 @@
-const modelos = { "S5732-H24S6Q": { descricao: "20x 1G, 4x 10G, 6x 40G", prefixos: [{ tipo: "1G", prefixo: "GigabitEthernet0/0/", limite: 20 }, { tipo: "10G",
-    prefixo: "XGigabitEthernet0/0/", limite: 4 }, { tipo: "40G", prefixo: "40GE0/0/", limite: 6 },] }, "S6730-H24X6C-100G": { descricao: "24x 10G, 6x 100G",
-    prefixos: [{ tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 24 }, { tipo: "100G", prefixo: "100GE0/0/", limite: 6 },] }, "S6730-H48X6C-100G": {
-    descricao: "48x 10G, 6x 100G", prefixos: [{ tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 48 }, { tipo: "100G", prefixo: "100GE0/0/", limite: 6
-    },] }, "S5720-36C-EI-28S-DC": { descricao: "28x 1G, 4x 10G", prefixos: [{ tipo: "1G", prefixo: "GigabitEthernet0/0/", limite: 28 }, { tipo: "10G",
-    prefixo: "XGigabitEthernet0/0/", limite: 4 },] }, "S6730-H24X6C-40G": { descricao: "24x 10G, 6x 40G", prefixos: [{ tipo: "10G", prefixo:
-    "XGigabitEthernet0/0/", limite: 24 }, { tipo: "40G", prefixo: "40GE0/0/", limite: 6 },] }, "S6730-H48X6C-40G": { descricao: "48x 10G, 6x 40G", prefixos:
-    [{ tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 48 }, { tipo: "40G", prefixo: "40GE0/0/", limite: 6 },] }, "S6750-H48Y8C": { descricao: "48x 10G,
-    8x 100G", prefixos: [{ tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 48 }, { tipo: "100G", prefixo: "100GE0/0/", limite: 8 },] }, "S6750-H48Y8C":
-    { descricao: "48x 10G, 8x 100G", prefixos: [{ tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 48 }, { tipo: "100G", prefixo: "100GE0/0/", limite: 8
-    }] }
+const modelos = {
+    "S5732-H24S6Q": {
+        descricao: "20x 1G, 4x 10G, 6x 40G",
+        prefixos: [
+            { tipo: "1G",  prefixo: "GigabitEthernet0/0/", limite: 20 },
+            { tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 4 },
+            { tipo: "40G", prefixo: "40GE0/0/", limite: 6 }
+        ]
+    },
+    "S6730-H24X6C-100G": {
+        descricao: "24x 10G, 6x 100G",
+        prefixos: [
+            { tipo: "10G",  prefixo: "XGigabitEthernet0/0/", limite: 24 },
+            { tipo: "100G", prefixo: "100GE0/0/", limite: 6 }
+        ]
+    },
+    "S6730-H48X6C-100G": {
+        descricao: "48x 10G, 6x 100G",
+        prefixos: [
+            { tipo: "10G",  prefixo: "XGigabitEthernet0/0/", limite: 48 },
+            { tipo: "100G", prefixo: "100GE0/0/", limite: 6 }
+        ]
+    },
+    "S5720-36C-EI-28S-DC": {
+        descricao: "28x 1G, 4x 10G",
+        prefixos: [
+            { tipo: "1G",  prefixo: "GigabitEthernet0/0/", limite: 28 },
+            { tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 4 }
+        ]
+    },
+    "S6730-H24X6C-40G": {
+        descricao: "24x 10G, 6x 40G",
+        prefixos: [
+            { tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 24 },
+            { tipo: "40G", prefixo: "40GE0/0/", limite: 6 }
+        ]
+    },
+    "S6730-H48X6C-40G": {
+        descricao: "48x 10G, 6x 40G",
+        prefixos: [
+            { tipo: "10G", prefixo: "XGigabitEthernet0/0/", limite: 48 },
+            { tipo: "40G", prefixo: "40GE0/0/", limite: 6 }
+        ]
+    },
+    "S6750-H48Y8C": {
+        descricao: "48x 10G, 8x 100G",
+        prefixos: [
+            { tipo: "10G",  prefixo: "XGigabitEthernet0/0/", limite: 48 },
+            { tipo: "100G", prefixo: "100GE0/0/", limite: 8 }
+        ]
+    }
 };
 
 export default modelos;
